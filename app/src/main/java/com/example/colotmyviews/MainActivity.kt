e774ba7591a92e86963ced0abf37fe16d4c401cc
Jasleen <Jasleen12112@gmail.com>
2023-02-13 15:03:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/colotmyviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplicatio
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/example/colotmyviews/MainActivity2.kt
@@ -0,0 +1,42 @@
+package com.example.colotmyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        val clickableViews : List<View> =
+            listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, Constraint_layout, red_button, yellow_button, green_button)
+
+        for(item in clickableViews){
+            item.setOnClickListener { makeColored(it) }
+        }
+
+    }
+
+    private fun makeColored(view : View){
+        when(view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.rgb(221, 127, 110))
+            R.id.box_two_text -> view.setBackgroundColor(Color.rgb(230, 143, 115))
+            R.id.box_three_text -> view.setBackgroundColor(Color.rgb(240, 153, 125))
+            R.id.box_four_text -> view.setBackgroundColor(Color.rgb(255, 195, 161))
+            R.id.box_five_text -> view.setBackgroundColor(Color.rgb(167, 93, 93))
+
+            R.id.red_button -> box_three_text.setBackgroundColor(Color.parseColor("#E54304"))
+            R.id.yellow_button -> box_four_text.setBackgroundColor(Color.parseColor("#ffc107"))
+            R.id.green_button -> box_five_text.setBackgroundColor(Color.parseColor("#12C700"))
+
+            else -> view.setBackgroundColor(Color.rgb(211, 117, 107))
+
+        }
+    }
+}
